f43a6343466be2771f701b10b465aa41cf7c4881
ghost1531 <81791684+ghost1531@users.noreply.github.com>
2024-08-08 10:57:55 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,136 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("../Projects/models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
+
+const mongoUrl = "mongodb://127.0.0.1:27017/wonderhomes"
+
+main()
+    .then(() => {
+        console.log("Connect to Database.");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+async function main() {
+    await mongoose.connect(mongoUrl);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req,res) =>{
+    res.send("Hey there, Welcome to My Dark World.");
+});
+
+const validateListing = (req,res,next) => {
+  let {error} = listingSchema.validate(req.body);
+  if(error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+}
+
+//Index Route 
+app.get("/listings", wrapAsync(async (req,res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+}));
+
+//New Route
+app.get("/listings/new", (req,res) => {
+    res.render("listings/new.ejs");
+});
+
+// Show Route
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ error: "Invalid ID" });
+  }
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+}));
+
+// Create Route
+  app.post("/listings", validateListing, wrapAsync(async (req, res) => {
+      const newListing = new Listing(req.body.listing);
+      await newListing.save();
+      res.redirect("/listings");    
+  })
+);
+
+// Edit Route
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ error: "Invalid ID" });
+  }
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+}));
+
+// Update Route
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const updatedListing = {
+      ...req.body.listing,
+      image: {
+        url: req.body.listing.image.url
+      }
+    };
+  
+    await Listing.findByIdAndUpdate(id, updatedListing);
+    res.redirect(`/listings/${id}`);
+  }));
+
+  
+// Delete Route
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ error: "Invalid ID" });
+  }
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+}));
+
+// app.get("/testListing", async (req,res) => {
+//     let sampleListing = new Listing ({
+//         title : "New Villa",
+//         description : "This is a new villa",
+//         price: 450,
+//         location: "Vancouver",
+//         country: "Canada",  
+//     });
+//     await sampleListing.save();
+//     console.log("Sample Saved");
+//     res.send("Sample Saved");
+// });
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page not Found."));
+})
+
+app.use((err, req,res, next) => {
+  let {statusCode = 500, message = "Something went Wrong !!"} = err;
+  res.status(statusCode).render("error.ejs", {message});
+  // res.status(statusCode).send(message);
+});
+
+app.listen(8080, () => {
+    console.log("Server is running on port 8080");
+});
